a6f13c9248ba79fd945ffa0fe9ca4aa9118f6e4d
AliAm2 <77967271+AliAm2@users.noreply.github.com>
2021-01-31 12:37:56 +0300

    Add files via upload

---
+++ Lab1  eXERCISE.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# $f(x)=a_{0}+\sum_{n=1}^{\infty}(a_{n}\cos\frac{n \pi x}{L}+b_{n}\sin\frac{n \pi x}{b})$
+
+# $(x+a)^{n}=\sum_{k=0}^{n}\binom{n}{k}x^{k}a^{n-k}$
